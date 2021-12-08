e353632dd0ea5b21ec3ab9d73f91a60292a0036a
Muhammad Sholeh <sholehbaabud11@gmail.com>
2021-12-08 09:02:11 +0700

    View Guest
    Landing Page
    Beranda
    Pelatihan

--- src/middlewares/employee.js
+++ src/middlewares/employee.js
@@ -1,42 +1,35 @@
-
-const database = require('../helper/database');
+const database = require("../helper/database");
 const helper = {
-    object : require('../helper/object')
-}
-const { columns } = require('../../config/config');
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
+  object: require("../helper/object"),
+};
+const { columns } = require("../../config/config");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
 module.exports = {
-    check : function(req, res, next){
-        const user_nip = req.session.user.nip;
-        database.query(
-            `SELECT * FROM employee_data WHERE nip=${user_nip}`,
-            (errors, results, fields) => {
-                if(results.length == 0)
-                return res.redirect('/user/employee-data');
-                else
-                next();
-            }
-        )
-    },
-    insert : function(req, res, next){
-        
-        const user = req.body;
-      
-        if( helper.object.isEmpty(user) )
-        return res.send('Missing some input.');
+  check: function (req, res, next) {
+    const user_nip = req.session.user.nip;
+    database.query(
+      `SELECT * FROM employee_data WHERE nip=${user_nip}`,
+      (errors, results, fields) => {
+        if (results.length == 0) return res.redirect("/user/employee-data");
+        else next();
+      }
+    );
+  },
+  insert: function (req, res, next) {
+    const user = req.body;
 
-        console.log(
-            user
-        );
+    if (helper.object.isEmpty(user)) return res.send("Missing some input.");
 
-        if(!helper.object.sameStructure(user,columns.employee_data))
-        return res.send('Wrong Format.');
+    console.log(user);
 
-        database.query(
-            `INSERT INTO employee_data VALUES(
+    if (!helper.object.sameStructure(user, columns.employee_data))
+      return res.send("Wrong Format.");
+
+    database.query(
+      `INSERT INTO employee_data VALUES(
                 '${req.session.user.nip}' 
                 ,'${user.name}' 
                 ,'${user.nik}' 
@@ -53,30 +46,23 @@ module.exports = {
                 ,'${user.department}' 
                 ,'${user.ranks}',
                 '${user.photo}'
-            )`, 
-            (errors, results, fields) => {
-                if(errors)
-                return res.send(errors);
-                else
-                next();
-            }
-        )
-    }, // Move to Authentication
-    update : function(req, res, next){
-        const user =  req.body;
-        const photoQuery = (
-            user.photo == undefined
-            ? ''
-            : `photo='${user.photo}',`
-        );
+            )`,
+      (errors, results, fields) => {
+        if (errors) return res.send(errors);
+        else next();
+      }
+    );
+  }, // Move to Authentication
+  update: function (req, res, next) {
+    const user = req.body;
+    const photoQuery = user.photo == undefined ? "" : `photo='${user.photo}',`;
 
-        if( helper.object.isEmpty(user) )
-        return res.send('Missing some input.');
+    if (helper.object.isEmpty(user)) return res.send("Missing some input.");
 
-        if(!helper.object.sameStructure(user, columns.employee_data))
-        return res.send(user);
+    if (!helper.object.sameStructure(user, columns.employee_data))
+      return res.send(user);
 
-        let query = `UPDATE employee_data 
+    let query = `UPDATE employee_data 
         SET 
         name='${user.name}',
         nik='${user.nik}',
@@ -94,87 +80,67 @@ module.exports = {
         ${photoQuery}
         ranks='${user.ranks}'
         WHERE nip='${req.session.user.nip}'`;
-        
-        database.query(
-            query,
-            (err, rows, fields) => {
-                if(err)
-                return res.send(err)
-                else
-                next()
-            }
-        )
-    },
-    get : function (req, res, next){
-        database.query(
-            `SELECT * FROM employee_data WHERE nip=${req.session.user.nip}`,
-            (err, row, fields) => {
-                if (err)
-                return res.send(err);
-                else
-                
-                if(row[0]==undefined)
-                row[0] = {};
 
-                res.locals.employee_data = row[0];
-                next();
-            }
-        )
-    },
-    storePhoto : function (){
-        const storage = multer.diskStorage({
-            destination : function (req , file, cb){
+    database.query(query, (err, rows, fields) => {
+      if (err) return res.send(err);
+      else next();
+    });
+  },
+  get: function (req, res, next) {
+    database.query(
+      `SELECT * FROM employee_data WHERE nip=${req.session.user.nip}`,
+      (err, row, fields) => {
+        if (err) return res.send(err);
+        else if (row[0] == undefined) row[0] = {};
 
-                const user_nip = req.session.user.nip;
+        res.locals.employee_data = row[0];
+        next();
+      }
+    );
+  },
+  storePhoto: function () {
+    const storage = multer.diskStorage({
+      destination: function (req, file, cb) {
+        const user_nip = req.session.user.nip;
 
-                const dir = path.resolve(`./assets/private/${user_nip}`);
-                const dirExist = fs.existsSync(dir);
+        const dir = path.resolve(`./assets/private/${user_nip}`);
+        const dirExist = fs.existsSync(dir);
 
-                console.log(
-                    'DIR exist : ' + dirExist
-                );
+        console.log("DIR exist : " + dirExist);
 
-                if(!dirExist)
-                fs.mkdirSync(dir, {recursive : true});
+        if (!dirExist) fs.mkdirSync(dir, { recursive: true });
 
-                cb(null, dir)
-            },
-            filename : function (req, file, cb ) {
+        cb(null, dir);
+      },
+      filename: function (req, file, cb) {
+        const user_nip = req.session.user.nip;
 
-                const user_nip = req.session.user.nip;
+        const ext = path.extname(file.originalname);
+        const fileName = user_nip + "_photo" + ext;
+        req.body.photo = fileName;
+        cb(null, fileName);
+      },
+    });
 
-                const ext = path.extname(file.originalname);
-                const fileName = user_nip+'_photo'+ext;
-                req.body.photo = fileName;
-                cb(null, fileName)
-            }
-        })
+    const upload = multer({ storage: storage });
+    const final = upload.single("photo");
+    return final;
+  },
+  restrictFile: function (req, res, next) {
+    const fileNip = req.params.fileName.split("_")[0];
+    console.log("File Nip : " + req.params.fileName.split("_")[0]);
+    const userNip = req.session.user.nip;
+    const isAutheticated = photoNip == userNip;
 
-        const upload = multer({storage : storage});      
-        const final = upload.single('photo')
-        return final;
-    },
-    restrictFile : function (req, res, next) {
-        
-        const fileNip = req.params.fileName.split('_')[0];
-        console.log(
-            'File Nip : ' + req.params.fileName.split('_')[0]
-        );
-        const userNip = req.session.user.nip;
-        const isAutheticated = ( photoNip == userNip);
+    if (!isAutheticated) return res.send("Unauthorized.");
 
-        if(!isAutheticated)
-        return res.send('Unauthorized.');
+    next();
+  },
+  getPhoto: function (req, res, next) {
+    const fileName = req.params.fileName;
+    const nip = req.session.user.nip;
+    const dir = path.resolve(`assets/private/${nip}/${fileName}`);
 
-        next()
-    },
-    getPhoto : function (req, res, next){
-        const fileName = req.params.fileName;
-        const nip = req.session.user.nip;
-        const dir = (
-            path.resolve(`assets/private/${nip}/${fileName}`)
-        );
-
-        res.sendFile(dir);
-    }
-}
+    res.sendFile(dir);
+  },
+};
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,62 +1,42 @@
-const router = require('express').Router();
-const path = require('path');
+const router = require("express").Router();
+const path = require("path");
 const middlewares = {
-    auth : require('../middlewares/auth'),
-    employee : require('../middlewares/employee'),
-    response : require('../middlewares/response'),
-    user : require('../middlewares/user')
-}
+  auth: require("../middlewares/auth"),
+  employee: require("../middlewares/employee"),
+  response: require("../middlewares/response"),
+  user: require("../middlewares/user"),
+};
 
 router
-.get(
-    '/register', [
-        middlewares.user.registerAccount
-    ]
-)
-.get(
-    '/home',[
-        middlewares.response.render('user/beranda.ejs')
-    ]
-    )
-    .get(
-        '/employee-data',
-        [
-            middlewares.auth.restrict,
-            middlewares.employee.get,
-            middlewares.response.render('user/employee-form.ejs')
-    ]
-)
-.get(
-    '/applications',
+  .get("/register", [middlewares.user.registerAccount])
+  .get("/home", [middlewares.response.render("user/beranda.ejs")])
+  .get("/employee-data", [
+    middlewares.auth.restrict,
+    middlewares.employee.get,
+    middlewares.response.render("user/employee-form.ejs"),
+  ])
+  .get(
+    "/applications",
     middlewares.auth.restrict,
     middlewares.user.getApplications,
-    middlewares.response.render('user/applications-list.ejs')
-)
-.get(
-    '/photo/:fileName',
-    middlewares.employee.getPhoto
-)
-.get(
-    '/notifications',
+    middlewares.response.render("user/applications-list.ejs")
+  )
+  .get("/photo/:fileName", middlewares.employee.getPhoto)
+  .get(
+    "/notifications",
     middlewares.user.getNotifications,
-    middlewares.response.render('user/notifikasi')
-)
-.post(
-    '/post-employee-data',
-    [
-        middlewares.auth.restrict,
-        middlewares.employee.storePhoto(),
-        middlewares.employee.insert,
-        middlewares.response.redirect('/user/home')
-    ]
-    )
-    .post(
-        '/update-employee-data',
-        [
-        middlewares.auth.restrict,
-        middlewares.employee.storePhoto(),
-        middlewares.employee.update,
-        middlewares.response.redirect('/user/employee-data')
-    ]
-)
+    middlewares.response.render("user/notifikasi")
+  )
+  .post("/post-employee-data", [
+    middlewares.auth.restrict,
+    middlewares.employee.storePhoto(),
+    middlewares.employee.insert,
+    middlewares.response.redirect("/user/home"),
+  ])
+  .post("/update-employee-data", [
+    middlewares.auth.restrict,
+    middlewares.employee.storePhoto(),
+    middlewares.employee.update,
+    middlewares.response.redirect("/user/employee-data"),
+  ]);
 module.exports = router;
